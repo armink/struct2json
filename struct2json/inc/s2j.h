9f03841c1c145a8d17bcb6f62d854aec69425185
armink <armink.ztl@gmail.com>
2016-07-20 08:34:17 +0800

    1、【更新】软件版本号至 1.0.2
    Signed-off-by: armink <armink.ztl@gmail.com>

--- struct2json/inc/s2j.h
+++ struct2json/inc/s2j.h
@@ -38,7 +38,7 @@ extern "C" {
 #endif
 
 /* struct2json software version number */
-#define S2J_SW_VERSION                "1.0.1"
+#define S2J_SW_VERSION                "1.0.2"
 
 /* Create JSON object */
 #define s2j_create_json_obj(json_obj) \
