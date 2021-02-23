42a45b8a414200f6f8711f654e3087ec1531377a
yuxuebao <yuxuebao@msn.com>
2021-02-23 16:55:22 +0800

    fix warnings

--- cJSON/cJSON.c
+++ cJSON/cJSON.c
@@ -96,9 +96,9 @@ CJSON_PUBLIC(const char *) cJSON_GetErrorPtr(void)
     return (const char*) (global_error.json + global_error.position);
 }
 
-CJSON_PUBLIC(char *) cJSON_GetStringValue(const cJSON * const item) 
+CJSON_PUBLIC(char *) cJSON_GetStringValue(const cJSON * const item)
 {
-    if (!cJSON_IsString(item)) 
+    if (!cJSON_IsString(item))
     {
         return NULL;
     }
@@ -106,9 +106,9 @@ CJSON_PUBLIC(char *) cJSON_GetStringValue(const cJSON * const item)
     return item->valuestring;
 }
 
-CJSON_PUBLIC(double) cJSON_GetNumberValue(const cJSON * const item) 
+CJSON_PUBLIC(double) cJSON_GetNumberValue(const cJSON * const item)
 {
-    if (!cJSON_IsNumber(item)) 
+    if (!cJSON_IsNumber(item))
     {
         return (double) NAN;
     }
@@ -310,7 +310,7 @@ static cJSON_bool parse_number(cJSON * const item, parse_buffer * const input_bu
     unsigned char number_c_string[64];
     unsigned char decimal_point = get_decimal_point();
     size_t i = 0;
-    cJSON_bool is_integer = 1;
+    cJSON_bool is_integer = true;
 
     if ((input_buffer == NULL) || (input_buffer->content == NULL))
     {
@@ -324,9 +324,6 @@ static cJSON_bool parse_number(cJSON * const item, parse_buffer * const input_bu
     {
         switch (buffer_at_offset(input_buffer)[i])
         {
-            case 'e':
-            case 'E':
-                is_integer = 0;
             case '0':
             case '1':
             case '2':
@@ -341,10 +338,14 @@ static cJSON_bool parse_number(cJSON * const item, parse_buffer * const input_bu
             case '-':
                 number_c_string[i] = buffer_at_offset(input_buffer)[i];
                 break;
-
+            case 'e':
+            case 'E':
+                number_c_string[i] = buffer_at_offset(input_buffer)[i];
+                is_integer = false;
+                break;
             case '.':
                 number_c_string[i] = decimal_point;
-                is_integer = 0;
+                is_integer = false;
                 break;
 
             default:
@@ -524,7 +525,7 @@ static unsigned char* ensure(printbuffer * const p, size_t needed)
 
             return NULL;
         }
-        
+
         memcpy(newbuffer, p->buffer, p->offset + 1);
         p->hooks.deallocate(p->buffer);
     }
@@ -1123,7 +1124,7 @@ CJSON_PUBLIC(cJSON *) cJSON_ParseWithLengthOpts(const char *value, size_t buffer
     }
 
     buffer.content = (const unsigned char*)value;
-    buffer.length = buffer_length; 
+    buffer.length = buffer_length;
     buffer.offset = 0;
     buffer.hooks = global_hooks;
 
@@ -2738,7 +2739,7 @@ CJSON_PUBLIC(cJSON *) cJSON_CreateStringArray(const char *const *strings, int co
     if (a && a->child) {
         a->child->prev = n;
     }
-    
+
     return a;
 }
 
